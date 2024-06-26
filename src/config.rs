944549b862248bf66ffd3a5e0ded906d3ff4dfc3
EastMonster <east_monster@126.com>
2024-06-26 22:06:13 +0800

    🦄 refactor: 开机启动的配置项不再保存在配置文件中

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
-use auto_launch::AutoLaunchBuilder;
-use once_cell::sync::OnceCell;
+use auto_launch::{AutoLaunch, AutoLaunchBuilder};
+use once_cell::sync::{Lazy, OnceCell};
 use serde::{Deserialize, Serialize};
 
 use scunet_login_util::*;
@@ -19,21 +19,26 @@ static CONFIG_FILE: OnceCell<String> = OnceCell::new();
 
 pub static ON_BOOT: OnceCell<bool> = OnceCell::new();
 
+static AUTO_LAUNCH_CONF: Lazy<AutoLaunch> = Lazy::new(|| {
+    AutoLaunchBuilder::new()
+        .set_app_name("AutoSCUNET")
+        .set_app_path(std::env::current_exe().unwrap().to_str().unwrap())
+        .set_args(&["--boot"])
+        .build()
+        .unwrap()
+});
+
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct AppConfig {
     pub student_id: String,
     pub password: String,
     pub service: Service,
+    #[serde(skip_serializing, skip_deserializing)]
     pub on_boot: bool,
 }
 
 pub fn on_boot_change(val: bool) {
-    let auto = AutoLaunchBuilder::new()
-        .set_app_name("AutoSCUNET")
-        .set_app_path(std::env::current_exe().unwrap().to_str().unwrap())
-        .set_args(&["--boot"])
-        .build()
-        .unwrap();
+    let auto = &AUTO_LAUNCH_CONF;
 
     if val { auto.enable() } else { auto.disable() }.unwrap_or_else(Toast::error);
 }
@@ -50,9 +55,11 @@ pub fn load_config() -> Result<AppConfig> {
         .set(pwd.join(CONFIG_FILE_NAME).to_str().unwrap().to_owned())
         .unwrap();
 
-    Ok(toml::from_str(&std::fs::read_to_string(
-        CONFIG_FILE.get().unwrap(),
-    )?)?)
+    let mut config: AppConfig =
+        toml::from_str(&std::fs::read_to_string(CONFIG_FILE.get().unwrap())?)?;
+    config.on_boot = AUTO_LAUNCH_CONF.is_enabled().unwrap();
+
+    Ok(config)
 }
 
 pub fn save_config(config: &AppConfig) -> Result<()> {
